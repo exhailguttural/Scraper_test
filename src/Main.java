5960c1b26ca8014f3dbc13bb01d80d812a099caa
exhailguttural@mail.ru <exhailguttural@mail.ru>
2014-08-12 22:02:29 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -8,50 +9,19 @@ import java.io.StringReader;
 import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.text.html.HTMLEditorKit;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		StringReader r = null;
-		try {
-			URL ur = new URL("http://www.cnn.com");
-			MyCallBack callBack = new MyCallBack();
-
-			r = new StringReader(getURLContent(ur));
-			HTMLEditorKit.Parser parser = new HTMLParse().getParser();
-			parser.parse(r, callBack, true);
-			
-			Writer writer = null;
-			try {
-			    writer = new BufferedWriter(new OutputStreamWriter(
-			    		new FileOutputStream("filename.txt"), "utf-8"));
-			    writer.write(callBack.getStr());
-			} catch (IOException ex) {
-				System.out.println("1");
-			} finally {
-				try {writer.close();} catch (Exception ex) {}
-			}
-			
-			String str = callBack.getStr();
-		    Pattern p = Pattern.compile("Gaza");
-		    Matcher m = p.matcher(str);
-		    int count = 0;
-		    while (m.find()){
-		    	count++;
-		    }
-		    System.out.println(count);
-		} catch (IOException ioex) {
-			System.out.println("2");
-		} finally {
-			r.close();
-		}
+		count(readUrl("urls.txt"), "Gaza");
 	}
 	
-	public static /*CharSequence*/String getURLContent(URL url) throws IOException {
+	public static String getURLContent(URL url) throws IOException {
 		URLConnection conn = url.openConnection();
 		String encoding = conn.getContentEncoding();
 		if (encoding == null) {
@@ -70,6 +40,86 @@ public class Main {
 			br.close();
 		}
 		return sb.toString();
-		//return sb;
+	}
+	
+	public static ArrayList<String> readUrl(String str) {
+		ArrayList<String> urlList = new ArrayList<>();
+		if (str.endsWith(".txt")) {
+			try (BufferedReader br = new BufferedReader(new FileReader(str)))
+			{
+				String sCurrentLine;
+	 
+				while ((sCurrentLine = br.readLine()) != null) {
+					urlList.add(sCurrentLine);
+				}
+	 
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		} else {
+			urlList.add(str);
 		}
+		return urlList;
+	}
+	
+	public static void count(ArrayList<String> urls, String wordToFind) {
+		int totalCount = 0;
+		int totalCharNumber = 0;
+		
+		for (String url : readUrl("urls.txt")) {
+			StringReader r = null;
+			try {
+				URL ur = new URL(url);
+				MyCallBack callBack = new MyCallBack();
+				
+				r = new StringReader(getURLContent(ur));
+				HTMLEditorKit.Parser parser = new HTMLParse().getParser();
+				parser.parse(r, callBack, true);
+				
+				Writer writer = null;
+				try {
+					writer = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream("filename.txt"), "utf-8"));
+					writer.write(callBack.getStr());
+				} catch (IOException ex) {
+					System.out.println("1");
+				} finally {
+					try {writer.close();} catch (Exception ex) {}
+				}
+				
+				String str = callBack.getStr();
+				Pattern p = Pattern.compile(wordToFind);
+				Matcher m = p.matcher(str);
+				int count = 0;
+				while (m.find()){
+					count++;
+				}
+				totalCount += count;
+				totalCharNumber += str.length();
+				printInfo(url, wordToFind, count, str.length(), true, true);
+			} catch (IOException ioex) {
+				System.out.println("IOException in count");
+			} finally {
+				if (r != null) {				
+					r.close();
+				}
+			}
+		}
+		printInfo("TOTAL", wordToFind, totalCount, totalCharNumber, true, true);
+	}
+	
+	public static void printInfo(String title
+							   , String word
+							   , int wordCount
+							   , int charNumber
+							   , boolean printOccurrence
+							   , boolean printCharNumber) {
+		System.out.println(title);
+		if (printOccurrence) {			
+			System.out.print("word " + word +" occurrences: " + wordCount + " ");
+		}
+		if (printCharNumber) {			
+			System.out.println("total number of characters " + charNumber);
+		}
+	}
 }
